70bacbd201cfb0715f6ea09df256c6bd1c6a00af
Lorys Lellouche <lorys.lellouche@gmail.com>
2024-06-18 10:31:39 +0200

    script abdou

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,8 @@ import Header from "./components/Header";
 import Dialog from "./components/Dialog";
 
 function App() {
-  const [pokemon, setPokemon] = useState(Math.floor(Math.random() * 1025) + 1);
+  const [pokemon, setPokemon] = useState(256);
+  const [pokemonDescription, setPokemonDescription] = useState("");
   const [pokemonName, setPokemonName] = useState("");
   const [count, setCount] = useState(0);
   const [day, setDay] = useState(new Date().toLocaleDateString());
@@ -42,10 +43,28 @@ function App() {
     fetchPokemon();
   }, [pokemon]);
 
+  useEffect(() => {
+    const fetchPokemonDescription = async () => {
+      try {
+        const response = await fetch(
+          `https://pokeapi.co/api/v2/pokemon-species/${pokemon}`
+        );
+        const data = await response.json();
+
+        setPokemonDescription(data.flavor_text_entries[3].flavor_text);
+
+        // console.log(data);
+      } catch (error) {
+        console.error("Error fetching data from json", error);
+      }
+    };
+    fetchPokemonDescription();
+  }, [pokemon]);
+
   const reset = () => {
     setPokemon(Math.floor(Math.random() * 1025) + 1);
   };
-
+  console.log(pokemonDescription);
   //   console.log(playAudio);
 
   return (
